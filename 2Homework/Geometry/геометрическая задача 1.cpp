dbbdc907ccfac55a1ccd5d01d6f94ddb6d808319
Timofey Leonchik <66067646+tima297@users.noreply.github.com>
2025-09-30 23:07:43 +0300

    Add files via upload

---
+++ 2Homework/Geometry/геометрическая задача 1.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	
+	int b = 1;
+	const int n=6;
+	int array[n];
+	for (int i = 0; i <n; i++) {
+		cout << "Write the x"<< b <<endl;
+		cin >> array[i];
+		cout << "Write the y" << b << endl;
+		cin >> array[i+=1];
+		b += 1;
+		
+	
+	}
+	for (int i = 0; i < n; i++) {
+		cout<<array[i];
+		
+	}
+	cout << " "<< endl;
+	float S =(abs(array[0]*(array[2]-array[5])+array[2]*(array[5]-array[1])+array[4]*(array[1]-array[3])))/2;
+	cout << "S=" << S;
+	return 0;
+}
+
