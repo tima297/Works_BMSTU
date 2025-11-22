0c0b4831a7ee62be8110dbd5fae0e63cda77d8da
Timofey Leonchik <66067646+tima297@users.noreply.github.com>
2025-11-22 08:38:20 +0300

    Add files via upload

---
+++ Labworks/Lab_work1/Лаб.Работа_1_ОАиП_В2.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    const int n = 10;
+    int array[n];
+    for (int i = 0; i < n; i++) {
+        cout << "write the " << i + 1 << " number:" << endl;
+        cin >> array[i];
+
+    }
+    for (int i = 0; i < n; i++) {
+        cout << array[i] << " ";
+    }
+    cout << " "<<endl;
+
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum = sum + array[i];
+    }
+    cout << "Summ of your elements " << sum << endl;
+
+
+    int max = array[0];
+    for (int i = 0; i < n; i++) {
+        if (array[i] > max) {
+            max = array[i];
+
+        }
+    }
+    cout << "The biggest element: " << max << endl;
+
+
+    void bubbleSort(int array[], int a); {
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9 - i; j++) {
+                if (array[j + 1] > array[j]) {
+                    int c = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = c;
+
+                }
+            }
+
+        }
+        for (int i = 0; i < n; i++) {
+            cout << array[i] << " ";
+        }
+    }
+
+
+
+    return 0;
+}
