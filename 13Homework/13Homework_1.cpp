c75f3c3edecf1b52a1d3e1390ce31ab63ed9dc01
Timofey Leonchik <66067646+tima297@users.noreply.github.com>
2025-12-09 15:11:24 +0300

    Add files via upload

---
+++ 13Homework/13Homework_1.cpp
@@ -0,0 +1,37 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+// Класс Книга
+class Book {
+private:
+    string title;    
+    string author;   
+    int year;     
+
+public:
+    // Конструктор - создает книгу с данными
+    Book(string t, string a, int y) {
+        title = t;
+        author = a;
+        year = y;
+    }
+
+    // Метод для вывода информации
+    void displayInfo() {
+        cout << "Название: " << title
+            << ", Автор: " << author
+            << ", Год выпуска: " << year << endl;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+    // Создаем книгу
+    Book myBook("Гарри Поттер", "Джоан Роулинг", 1997);
+
+    // Выводим информацию
+    myBook.displayInfo();
+
+    return 0;
+}
---
+++ 13Homework/13Homework_2.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+using namespace std;
+
+class Rectangle {
+private:
+    double length;  
+    double width;  
+
+public:
+    Rectangle(double l, double w) {
+        length = l;
+        width = w;
+    }
+
+    // Метод для вычисления площади
+    double calculateArea() {
+        return length * width;
+    }
+
+    // Метод для вычисления периметра
+    double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+
+    void displayInfo() {
+        cout << "Длина: " << length
+            << ", Ширина: " << width
+            << ", Площадь: " << calculateArea()
+            << ", Периметр: " << calculatePerimeter() << endl;
+    }
+};
+
+int main() {
+    setlocale(LC_ALL, "Russian");
+
+    // Создаем прямоугольник
+    Rectangle myRect(5.5, 3.2);
+
+    myRect.displayInfo();
+
+    return 0;
+}
